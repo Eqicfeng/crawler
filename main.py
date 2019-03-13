497cc9d834bfb1bca779defb5af1ec4337454399
Eqicfeng <870876189@qq.com>
2019-03-13 19:47:02 +0800

    store data in mysql

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import pymysql
+
+# 打开数据库连接
+db = pymysql.connect("localhost", "root", "", "test")
+cursor = db.cursor()
 
 url = "https://stackoverflow.com/questions/tagged/android"
+
 url_votes = url+"?sort=votes&days=7"
 print(url_votes)
 f = requests.get(url_votes)
@@ -10,6 +16,8 @@ soup = BeautifulSoup(f.content, "lxml")
 all_votes = soup.find_all('div',class_='question-summary')
 for k in range(10):
    a = all_votes[k].find_all('a')
+   cursor.execute("INSERT INTO `newest` VALUES ('%s','%s')" % (a[0].string, a[0]['href']))
+   #db.commit()
    print(a[0]['href'])
    print(a[0].string)
 
@@ -18,8 +26,15 @@ print(url_newest)
 f = requests.get(url_newest)
 soup = BeautifulSoup(f.content, "lxml")
 
-all_votes = soup.find_all('div',class_='question-summary')
+all_newest = soup.find_all('div',class_='question-summary')
 for k in range(10):
-   a = all_votes[k].find_all('a')
+   a = all_newest[k].find_all('a')
+   cursor.execute("INSERT INTO `votes` VALUES ('%s','%s')" % (a[0].string, a[0]['href']))
+   #db.commit()
    print(a[0]['href'])
    print(a[0].string)
+
+
+db.commit()
+# 关闭数据库连接
+db.close()
